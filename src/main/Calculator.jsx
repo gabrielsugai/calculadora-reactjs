530ae4b029c8ecf2864d0c26b034ebeaa872ddf6
Gabriel Sugai <gabrielsugai@gmail.com>
2020-09-16 14:38:16 -0300

    Adiciona logica das operaçoes matematicas

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -8,7 +8,7 @@ const initialState = {
   displayValue: '0',
   clearDisplay: false,
   operation: null,
-  value: [0, 0],
+  values: [0, 0],
   current: 0
 }
 
@@ -28,6 +28,28 @@ export default class Calculator extends Component {
   }
 
   setOperation(operation){
+    if(this.state.current === 0){
+      this.setState({ operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = operation === '='
+      const currentOperation = this.state.operation
+
+      const values = [...this.state.values]
+      try{
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+      } catch(e){
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
 
   }
 
@@ -45,8 +67,7 @@ export default class Calculator extends Component {
     if (n !== '.') {
       const i = this.state.current
       const newValue = parseFloat(displayValue)
-      const values = [this.state.values]
-      console.log(values)
+      const values = [...this.state.values]
       values[i] = newValue
       this.setState({ values })
       console.log(values)
